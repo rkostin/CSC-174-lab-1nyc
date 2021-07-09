f6bb49bc5703bccb860cbcc114fcf92c1de71f95
Robert M Kostin <3536295+rkostin@users.noreply.github.com>
2021-07-09 18:09:59 -0400

    Init

---
+++ brooklyn.php
@@ -0,0 +1,37 @@
+<?php include "inc/html-top.php"; ?>
+
+<body>
+<div class="container">
+
+  <header class="page-header">
+    <h1>
+      <a href="index.php">New York City</a>
+    </h1>
+  </header>
+
+<?php include "inc/nav.php"; ?>
+
+  <section>
+    <h2>Brooklyn</h2>
+    <img src="images/brooklyn.jpg" alt="Brooklyn Bridge"></a>
+  </section>
+
+  <section>
+    <h3>Geography</h3>
+    <p>Brooklyn is the most populous of New York City’s five boroughs, with about 2.5 million people, and the second-largest in area. Since 1896, Brooklyn has had the same boundaries as Kings County, which is now the most populous county in New York and the second-most densely populated county in the United States, after New York County (Manhattan). It is also the westernmost county on Long Island. Today, if it were an independent city, Brooklyn would rank as the fourth most populous city in the U.S., behind only the other boroughs of New York City combined, Los Angeles, and Chicago.</p>
+  </section>
+
+  <section>
+    <h3>Characteristics</h3>
+    <p>Brooklyn was an independent city until January 1, 1898 when, according to the Charter of “Greater New York,” Brooklyn was consolidated with the other boroughs to form the modern “City of New York.” It continues to maintain a distinct culture. Many Brooklyn neighborhoods are ethnic enclaves where particular ethnic groups and cultures predominate. Brooklyn’s official motto is Eendraght Maeckt Maght. Written in early modern Dutch, it is translated, “In unity, there is strength.” The motto is displayed on the borough seal and flag, which also feature a young robed woman bearing fasces, a traditional emblem of Republicanism. Brooklyn’s official colors are blue and gold.</p>
+  </section>
+
+  <footer class="page-footer">
+    <a href="http://urcsc174.org" target="_blank">Advanced Front-End Web Design</a>
+    <div>Content arrangement from <a href="https://cs.nyu.edu/~kapp" target="_blank">Craig M. Kapp</a>. Content from <a href="https://www.wikipedia.org/" target="_blank">WikiPedia</a></div>
+  </footer>
+
+<?php include "inc/scripts.php"; ?>
+</div> <!-- close .container -->
+</body>
+</html>
